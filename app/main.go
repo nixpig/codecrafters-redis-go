e7adb3b85721d80671a88f6c09a47a9132a96c50
nixpig <143995476+nixpig@users.noreply.github.com>
2025-08-02 05:51:48 +0100

    feat: pong

--- app/main.go
+++ app/main.go
@@ -4,22 +4,36 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"os/signal"
 )
-
-var _ = net.Listen
-var _ = os.Exit
 
 func main() {
 	fmt.Println("Logs from your program will appear here!")
 
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	listener, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+	defer listener.Close()
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, os.Kill)
+
+	go func() {
+		for {
+			conn, err := listener.Accept()
+			if err != nil {
+				fmt.Println("Error accepting connection: ", err.Error())
+				os.Exit(1)
+			}
+
+			if _, err := conn.Write([]byte("+PONG\r\n")); err != nil {
+				fmt.Println(err)
+			}
+		}
+	}()
+
+	<-done
+	fmt.Println("Exiting...")
 }
